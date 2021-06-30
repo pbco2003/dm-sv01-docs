d7b2200099b0f92178a0b588e4e432487614f1a3
Edgar Piacentini <edgar.piacentini@gmail.com>
2021-06-30 00:57:12 -0300

    blue header

--- website/src/theme/Playground.js
+++ website/src/theme/Playground.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+/*
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import prettier from 'prettier/standalone';
@@ -32,3 +32,4 @@ function Playground(props) {
 }
 
 export default Playground;
+*/
